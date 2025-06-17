9ae42ac76e84682320e3c39e4971980b3459110d
ssu <tngus015@naver.com>
2025-06-17 20:49:42 +0900

    exercised_at, created_at null 문제 해결

--- src/main/java/com/myfitmate/myfitmate/domain/exercise/service/ExerciseLogService.java
+++ src/main/java/com/myfitmate/myfitmate/domain/exercise/service/ExerciseLogService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -42,8 +43,13 @@ public class ExerciseLogService {
                 .exercise(exercise)
                 .durationMinutes(duration)
                 .kcalBurned(kcal)
-                .exerciseAt(dto.getExerciseAt())
+                .exerciseAt(
+                        dto.getExerciseAt() != null
+                                ? dto.getExerciseAt()
+                                : LocalDateTime.now()
+                )
                 .build();
+
 
         exerciseLogRepository.save(log);
     }
